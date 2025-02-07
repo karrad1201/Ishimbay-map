789249a0011b629e8022a2c0234e6a57992dfe39
karrad1201 <karad120109@gmail.com>
2025-02-08 03:00:54 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template, request  # Добавляем request
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 static_folder = os.path.abspath("static")  # Определяем static_folder здесь
 app = Flask(__name__, template_folder="templates", static_folder=static_folder)  # Передаем static_folder в Flask
 
